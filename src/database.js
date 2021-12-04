1d441b988c85ff768d383a8c9e2cd270db512470
EdithGarciaMa <79724436+EdithGarciaMa@users.noreply.github.com>
2021-12-03 22:02:39 -0600

    se agrego la parte del login, registro y modificar registros

--- src/database.js
+++ src/database.js
@@ -1,11 +1,29 @@
-const mongoose = require('mongoose')
+//const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://mongo/mydatabase'), {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifieldTopology: true,
-    useFindAndModify: false
-}
-    .then(db => console.log('Db is connected to', db.connection.host))
-    .catch(err => console.error(err));
+//mongoose.connect('mongodb://mongo/notesdb'), {
+//    useNewUrlParser: true,
+//    useCreateIndex: true,
+//    useUnifieldTopology: true,
+//    useFindAndModify: false
+//}
+//    .then(db => console.log('Db is connected to', db.connection.host))
+//    .catch(err => console.error(err));
+
+
+import mongoose from "mongoose";
+import config from "./config";
+    
+    (async () => {
+      try {
+        const db = await mongoose.connect(config.MONGODB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useFindAndModify: false,
+          useCreateIndex: true,
+        });
+        console.log("Mongodb is connected to", db.connection.host);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
     
--- src/index.js
+++ src/index.js
@@ -1,10 +1,82 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('express-session');
+//conts flash = require('connect-flash');
 
+
+//const app = express();
+
+//require('./database');
+
+//app.use(require('./routes/index.routes'))
+
+//app.listen(3000);
+//console.log('Servidor en puerto', 3000);
+
+
+//import app from "./app";
+//import "./database";
+
+// Server is listening
+//app.listen(app.get("port"));
+
+//console.log("Server on port", app.get("port"));
+//console.log("Environment:", process.env.NODE_ENV);
+
+
+
+// Initializations
 const app = express();
+require('./database')
+//createAdminUser();
 
-require('./database');
+// settings
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(".hbs",exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
 
-app.use(require('./routes/index.routes'))
+// middlewares
+//app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+ //   store: MongoStore.create({ mongoUrl: config.MONGODB_URI }),
+  })
+);
+//app.use(passport.initialize());
+//app.use(passport.session());
+app.use(flash());
 
-app.listen(3000);
-console.log('Servidor en puerto', 3000);
+// Global Variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
+  next();
+});
+
+// routes
+app.use(indexRoutes);
+app.use(userRoutes);
+app.use(notesRoutes);
+
+// static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res) => {
+  res.render("404");
+});
+
+export default app;
